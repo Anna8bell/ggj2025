87d375f14f6d6b635726d5eccd8f99b54be80bc8
Kirill Shchegolev <shegolev2000@gmail.com>
2025-01-24 00:36:17 +0200

    Firewall object added

--- Assets/Scripts/Manager.cs
+++ Assets/Scripts/Manager.cs
@@ -8,6 +8,7 @@ public class Manager : MonoBehaviour
     public RoomLevel nextLevel;
 
     public Dragon dragon;
+    public GameObject fireWall;
 
     
     // Start is called once before the first execution of Update after the MonoBehaviour is created
