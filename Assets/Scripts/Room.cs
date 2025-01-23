bfd84d7fe10da645e845035b81b31395bc435948
Kirill Shchegolev <shegolev2000@gmail.com>
2025-01-23 23:07:47 +0200

    Level added

---
+++ Assets/Scripts/Room.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class Room : MonoBehaviour
+{
+    public GameObject mainSlot;
+    public GameObject combatSlot1;
+    public GameObject combatSlot2;
+    public GameObject itemSlot1;
+    public GameObject itemSlot2;
+
+    public GameObject exit;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/RoomLevel.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class RoomLevel : MonoBehaviour
+{
+    public Room room1;
+    public Room room2;
+    public Room room3;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
